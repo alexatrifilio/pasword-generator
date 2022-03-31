e79853847063e1731c0f2f030985395f07810b83
Vanessa <vaneira24@gmail.com>
2022-03-30 21:43:28 -0300

    Creation of layout elements

---
+++ script.js
@@ -0,0 +1,87 @@
+
+// ---Create elements--- //
+
+const body = document.getElementById('body');
+const mainTitle = document.createElement('h1');
+const mainTitleText = document.createTextNode('Generador de Contraseñas');
+
+// Final Password Card //
+const passContainer = document.createElement('div');
+const iconContainer = document.createElement('div');
+const genPass = document.createTextNode('');
+const copyIcon = document.createElement('i');
+const refreshIcon = document.createElement('i');
+
+// Form Card //
+const formContainer = document.createElement('div');
+const title = document.createElement('h2');
+const titleText = document.createTextNode('Personalice su contraseña');
+
+    // Lenght section //
+const lenghtContainer = document.createElement('div');
+const lenghtTitle = document.createElement('h3');
+const lenghtTitleText = document.createTextNode('Longitud');
+
+        // Lenght Form //
+const inputCont1 = document.createElement('div');
+const inputCont2 = document.createElement('div');
+const inputCont3 = document.createElement('div');
+const lenghtInput1 = document.createElement('input');
+const lenghtInput2 = document.createElement('input');
+const lenghtInput3 = document.createElement('input');
+const lenghtLabel1 = document.createElement('label');
+const lenghtLabel2 = document.createElement('label');
+const lenghtLabel3 = document.createElement('label');
+const lenghtLabelText1 = document.createTextNode('12 caracteres');
+const lenghtLabelText2 = document.createTextNode('9 caracteres');
+const lenghtLabelText3 = document.createTextNode('6 caracteres');
+
+
+    // Rules section //
+const rulesContainer = document.createElement('div');
+const rulesTitle = document.createElement('h3');
+const rulesTitleText = document.createTextNode('Reglas');
+
+        // Rules Form //
+const inputCont4 = document.createElement('div');
+const inputCont5 = document.createElement('div');
+const inputCont6 = document.createElement('div');
+const rulesInput1 = document.createElement('input');
+const rulesInput2 = document.createElement('input');
+const rulesInput3 = document.createElement('input');
+const rulesLabel1 = document.createElement('label');
+const rulesLabel2 = document.createElement('label');
+const rulesLabel3 = document.createElement('label');
+const rulesLabelText1 = document.createTextNode('Solo letras');
+const rulesLabelText2 = document.createTextNode('Lectura Simple');
+const rulesLabelText3 = document.createTextNode('Todos los caracteres');
+
+    //Characters Section //
+const charactContainer = document.createElement('div');
+const charactTitle = document.createElement('h3');
+const charactTitleText = document.createTextNode('Caracteres');
+
+        // Characters Form //
+const inputCont7 = document.createElement('div');
+const inputCont8 = document.createElement('div');
+const inputCont9 = document.createElement('div');
+const inputCont10 = document.createElement('div');
+const charactInput1 = document.createElement('input');
+const charactInput2 = document.createElement('input');
+const charactInput3 = document.createElement('input');
+const charactInput4 = document.createElement('input');
+const charactLabel1 = document.createElement('label');
+const charactLabel2 = document.createElement('label');
+const charactLabel3 = document.createElement('label');
+const charactLabel4 = document.createElement('label');
+const charactLabelText1 = document.createTextNode('Mayúsculas');
+const charactLabelText2 = document.createTextNode('Minúsculas');
+const charactLabelText3 = document.createTextNode('Números');
+const charactLabelText4 = document.createTextNode('Símbolos');
+
+
+
+// ---Append element--- //
+
+
+// ---Set attribute elements--- //
